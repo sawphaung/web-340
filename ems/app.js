53e37685aa7bdae3bc85afca9f04a549613d2868
Saw Phaung <saw@77creative.io>
2022-02-26 16:20:21 -0600

    Cross-site Request Forgery

--- ems/app.js
+++ ems/app.js
@@ -8,28 +8,24 @@ Date: Feb 06, 2022
 ===========================================
 */
 var express = require("express");
-
 var http = require("http");
-
 var path = require("path");
-
 var logger = require("morgan");
-
 var helmet = require("helmet"); // Library for XSS attacks
-
-var app = express();
+var bodyParser = require("body-parser"); // CSFR 
+var cookieParser = require("cookie-parser"); // CSFR
+var csrf = require("csurf"); // CSFR 
+var csrfProtection = csrf({cookie: true}); // Setup csrf Protection
 
 // Mongoose Connection
 var mongoose = require("mongoose");
-
 var Employee = require('./models/employee');
-
+const { cookie } = require("express/lib/response");
 // database connection string to MongoDB Atlas
 const mongoDB = "mongodb+srv://admin:5yBXoiUxHw7MA8Mh@buwebdev-cluster-1.azwcv.mongodb.net/ems";
 
 // DB Connection
 mongoose.connect(mongoDB);
-
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 // Error
@@ -39,14 +35,30 @@ db.once("open", function(){
     console.log("Application connected to mLab MongoDB instance");
 })
 
-
-app.set("views", path.resolve(__dirname, "views"));
-
-app.set("view engine", "ejs");
-
+// initialize the express Application
+var app = express();
 app.use(logger("short"));
 
+// Security
 app.use(helmet.xssFilter());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+app.use(cookieParser());
+app.use(csrfProtection);
+
+app.use(function(request, response, next){
+    var token = request.csrfToken();
+    response.cookie("XSRF-Token", token);
+    response.locals.csrfToken = token;
+    next();
+});
+
+// View Engines
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
 
 // Get Images
 app.use(express.static('public')); 
@@ -56,25 +68,29 @@ app.use('/css',express.static('css'));
 
 app.get("/", function(request, response) {
     response.render("index", {
-        title: "Home Page"
+        title: "Home Page",
+        message: "EMS"
     });
 });
 
 app.get("/new", function(request, response) {
-    response.render("index", {
-        title: "Data Entry Page"
+    response.render("new", {
+        title: "Data Entry Page",
+        message: "Add New Employee",
     });
 });
 
 app.get("/list", function(request, response) {
-    response.render("index", {
-        title: "Employee Records"
+    response.render("list", {
+        title: "Employee Records",
+        message: "Employee Lists",
     });
 });
 
 app.get("/view", function(request, response) {
-    response.render("index", {
-        title: "View Employee Records"
+    response.render("view", {
+        title: "View Employee Records",
+        message: "View Employee"
     });
 });
 
